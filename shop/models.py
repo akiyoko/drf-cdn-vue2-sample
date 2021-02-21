e3fcda59f71ade0ba0f8e58c05ab6f001ba54f0d
akiyoko <akiyoko@users.noreply.github.com>
2021-02-21 17:45:26 +0900

    2021.2 Update

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,6 @@
 import uuid
 
 from django.db import models
-from django.utils import timezone
 
 
 class Book(models.Model):
@@ -12,8 +11,9 @@ class Book(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(verbose_name='タイトル', max_length=20)
-    price = models.IntegerField(verbose_name='価格', null=True)
-    created_at = models.DateTimeField(default=timezone.now)
+    price = models.IntegerField(verbose_name='価格', null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name='登録日時',
+                                      auto_now_add=True)
 
     def __str__(self):
         return self.title
